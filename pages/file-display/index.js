86f53dc4aedf066cba8962b8514c9a9096f31224
yoelgal <yoelgal108@gmail.com>
2022-12-09 15:22:18 +0000

    commit

--- pages/file-display/index.js
+++ pages/file-display/index.js
@@ -1,18 +1,29 @@
 import {Document} from 'react-pdf'
 import {useRouter} from 'next/router';
+import React, {useState, useLayoutEffect} from 'react';
+import {paths} from "../../utils/utils"
+
 
 const MyComponent = () => {
     const router = useRouter();
+    const [iframeHeight, setIframeHeight] = useState(0);
+
+    useLayoutEffect(() => {
+        setIframeHeight(window.innerHeight);
+    }, []);
 
     // the URL of the PDF file
     // const pdfUrl = 'https://www.mathsgenie.co.uk/resources/as-pure-algebraic-expressions.pdf'
 
     const id = router.query.id
+
+
     return (
         <>
             {/*<h1>Hello</h1>*/}
-            {/*<p>The current file id is: {router.query.id}</p>*/}
-            <iframe src={`https://github.com/yoelgal/cs-wizard/blob/main/utils/data/p1questions1.pdf`} width="100%" height="1000px"></iframe>
+            <p>The current file num is: {paths[id]}</p>
+            <iframe src={`https://docdro.id/ImuNldF`} width="100%" height={iframeHeight}
+                    style={{maxHeight: `${iframeHeight}px`}}></iframe>
         </>
     )
 }
