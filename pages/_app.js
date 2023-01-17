b4dd8740a6bb2cbd2cdb159a309216a73e9c4854
yoelgal <yoelgal108@gmail.com>
2023-01-17 14:16:23 +0000

    analystics installed

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 import '../styles/globals.css'
 import Layout from "../layout/layout";
-
+import {Analytics} from '@vercel/analytics/react';
 
 
 function MyApp({Component, pageProps}) {
 
 
-    return <Layout><Component {...pageProps} /></Layout>
+    return(
+            <Layout>
+                <Component {...pageProps} />
+                <Analytics></Analytics>
+            </Layout>
+        )
+
 
 }
 
