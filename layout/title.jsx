039985a5a0f243d684b8511a6dfdc9705e9a66f8
yoelgal <yoelgal108@gmail.com>
2023-06-18 10:08:31 +0100

    Title change

--- layout/title.jsx
+++ layout/title.jsx
@@ -1,41 +1,54 @@
-import {Header} from "semantic-ui-react";
+import React, { useEffect, useState } from "react";
+import { Header } from "semantic-ui-react";
 import styles from "../styles/Home.module.css";
-import React, {useEffect, useState} from "react";
 
+const Title = () => {
+  let clicked = false;
 
-const Title = ()=>{
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const titles = [
+    "<span>CS Wizard</span>",
+    "<span>// CS Wizard</span>",
+    "<span>/* CS Wizard */</span>",
+    "<span># CS Wizard</span>",
+    "<span>CSWizard()</span>",
+    "<span>CS.Wizard.js</span>",
+    "<span>! CS Wizard</span>",
+    "<span>CSWizard;</span>",
+    "<span>-- CS Wizard</span>",
+    '<span>"" || CS Wizard</span>',
+  ];
+  let titleNum;
 
-    let clicked = false
+  const [title, setTitle] = useState("");
 
+  useEffect(() => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    const titles = [ "<span>CS Wizard</span>", "<span>// CS Wizard</span>", "<span>/* CS Wizard */</span>", '<span># CS Wizard</span>','<span>CSWizard()</span>','<span>CS.Wizard.js</span>','<span>! CS Wizard</span>','<span>CSWizard;</span>','<span>-- CS Wizard</span>','<span>"" || CS Wizard</span>', "<span>CS <em>Rizzard</em></span>"]
-    let titleNum
+    titleNum = Math.floor(Math.random() * titles.length);
+    const titleDiv = document.getElementById("title");
+    titleDiv.innerHTML = titles[titleNum];
+  }, [titles]);
 
-
-    const [title, setTitle] = useState("")
-
-    useEffect(()=>{
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        titleNum = Math.floor(Math.random() * titles.length)
-        const titleDiv = document.getElementById('title')
-        titleDiv.innerHTML = titles[titleNum]
-    }, [titles])
-
-    const titleClick = ()=>{
-        const titleDiv = document.getElementById('title')
-        if (titleNum < titles.length - 1) {
-            titleNum++
-        } else {
-            titleNum = 0
-        }
-        titleDiv.innerHTML = titles[titleNum]
+  const titleClick = () => {
+    const titleDiv = document.getElementById("title");
+    if (titleNum < titles.length - 1) {
+      titleNum++;
+    } else {
+      titleNum = 0;
     }
+    titleDiv.innerHTML = titles[titleNum];
+  };
 
+  return (
+    <Header
+      as="h1"
+      id={"title"}
+      className={styles.title}
+      onClick={() => titleClick()}
+    >
+      {title}
+    </Header>
+  );
+};
 
-
-    return (
-        <Header as="h1" id={'title'} className={styles.title} onClick={()=>titleClick()}>{title}</Header>
-    )
-}
-
-export default Title
+export default Title;
